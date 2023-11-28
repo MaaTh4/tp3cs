093500a926cebcb5d7c3872fe39be9a81e75bdf9
MaaTh4 <127776316+MaaTh4@users.noreply.github.com>
2023-11-27 22:28:52 -0300

    Create Autor.cs

---
+++ Autor.cs
@@ -0,0 +1,12 @@
+namespace Tp3.classes
+{
+    public class Autor
+    {
+
+        public void livroLançado()
+        {
+            Console.WriteLine("O livro foi publicado");
+        }
+
+    }
+}
